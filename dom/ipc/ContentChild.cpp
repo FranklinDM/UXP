d0b61d4a68961463c7f7738efcc0fb8acc184f16
Moonchild <moonchild@palemoon.org>
2026-07-24 19:29:00 +0200

    No issue - CallUpdateWindow is [[nodiscard]], but we don't need to use the result here.

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -2455,7 +2455,7 @@ ContentChild::RecvUpdateWindow(const uintptr_t& aChildId)
   if (parentInstance) {
   // sync! update call to the plugin instance that forces the
   // plugin to paint its child window.
-  parentInstance->CallUpdateWindow();
+  Unused << parentInstance->CallUpdateWindow();
   }
   return true;
 #else
