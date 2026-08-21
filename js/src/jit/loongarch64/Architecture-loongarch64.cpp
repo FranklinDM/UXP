4a825adf7d333d52df983ab325bf432594ab7a77
Basilisk-Dev <basiliskdev@protonmail.com>
2026-08-21 19:00:34 -0400

    Issue #3049 - Account for LoongArch64 SIMD FP aliases

--- js/src/jit/loongarch64/Architecture-loongarch64.cpp
+++ js/src/jit/loongarch64/Architecture-loongarch64.cpp
@@ -16,6 +16,11 @@
 namespace js {
 namespace jit {
 
+static uint32_t CountPhysBits(FloatRegisters::SetType bits) {
+  MOZ_ASSERT((bits & ~FloatRegisters::AllPhysMask) == 0);
+  return mozilla::CountPopulation32(uint32_t(bits));
+}
+
 Registers::Code Registers::FromName(const char* name) {
   for (size_t i = 0; i < TotalPhys; i++) {
     if (strcmp(GetName(i), name) == 0) {
@@ -37,23 +42,42 @@ FloatRegisters::Code FloatRegisters::FromName(const char* name) {
 }
 
 FloatRegisterSet FloatRegister::ReduceSetForPush(const FloatRegisterSet& s) {
-  LiveFloatRegisterSet ret;
-  for (FloatRegisterIterator iter(s); iter.more(); ++iter) {
-    ret.addUnchecked(FromCode((*iter).encoding()));
-  }
-  return ret.set();
+  SetType bits = s.bits();
+
+  // Exclude registers already represented by a wider alias. Higher alias banks
+  // are wider, so preserve only the widest live view of each physical register.
+  bits &= ~(bits >> (1 * FloatRegisters::TotalPhys));
+  bits &= ~(bits >> (2 * FloatRegisters::TotalPhys));
+
+  return FloatRegisterSet(bits);
 }
 
 uint32_t FloatRegister::GetSizeInBytes(const FloatRegisterSet& s) {
-  return s.size() * sizeof(double);
+  return s.size() * sizeof(FloatRegisters::RegisterContent);
 }
 
 uint32_t FloatRegister::GetPushSizeInBytes(const FloatRegisterSet& s) {
-  return s.size() * sizeof(double);
+  SetType all = s.bits();
+  SetType set128b =
+      (all >> (uint32_t(FloatRegisters::Simd128) * FloatRegisters::TotalPhys)) &
+      FloatRegisters::AllPhysMask;
+  SetType doubleSet =
+      (all >> (uint32_t(FloatRegisters::Double) * FloatRegisters::TotalPhys)) &
+      FloatRegisters::AllPhysMask;
+  SetType singleSet =
+      (all >> (uint32_t(FloatRegisters::Single) * FloatRegisters::TotalPhys)) &
+      FloatRegisters::AllPhysMask;
+
+  SetType set64b = doubleSet & ~set128b;
+  SetType set32b = singleSet & ~set64b & ~set128b;
+
+  return CountPhysBits(set128b) * (4 * sizeof(int32_t)) +
+         CountPhysBits(set64b) * sizeof(double) +
+         CountPhysBits(set32b) * sizeof(float);
 }
 
 uint32_t FloatRegister::getRegisterDumpOffsetInBytes() {
-  return encoding() * sizeof(double);
+  return encoding() * sizeof(FloatRegisters::RegisterContent);
 }
 
 bool CPUFlagsHaveBeenComputed() {
